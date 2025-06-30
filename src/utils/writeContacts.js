e05987b5413fc1ce8ebba41e08b78e8e74c626f5
tetiana_hushnovska <thushnyovska@gmail.com>
2025-06-30 14:10:36 +0100

    fix file path

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -2,10 +2,5 @@ import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
 export const writeContacts = async (updatedContacts) => {
-    try {
-        await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, undefined, 2));
-    }
-    catch (err) {
-        console.error('Error writing contacts:', err.message);
-    }
+        await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));  
 };
